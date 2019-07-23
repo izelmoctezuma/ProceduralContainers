0682c4f167ae707301c5eea9db9a6ae3a3debc86
Izel Moctezuma <izelmoctezuma+github@gmail.com>
2019-07-22 21:19:33 -0500

    Initial commit

---
+++ Source/Containers/Containers.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Containers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Containers, "Containers" );
